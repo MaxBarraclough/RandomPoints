7586209185ef774513fc55a85d897ec916059aab
Max Barraclough <maxbarraclough@gmail.com>
2019-02-23 17:46:46 +0000

    Implement + expose the unbiased (sqrt) selector

--- src/randompoints/RandomPointsFXMLController.java
+++ src/randompoints/RandomPointsFXMLController.java
@@ -61,6 +61,39 @@ public final class RandomPointsFXMLController implements Initializable {
 
     }
 
+
+    @FXML
+    private void goSqrt(ActionEvent event) {
+        System.out.println("Now I will draw to the canvas...");
+
+        final GraphicsContext graphCon = canvas.getGraphicsContext2D();
+        graphCon.setFill(Color.WHITE);
+        graphCon.setStroke(Color.WHITE);
+
+        for (int i = 0 ; i != 200; ++i)
+        {
+            // ignore that it should be [0.0, 1.0) not [0.0 to 1.0] from nextDouble
+            final double theta_radians
+                    = RandomPointsFXMLController.Rand.nextDouble()
+                    * RandomPointsFXMLController.TAU;
+
+            final double seed = RandomPointsFXMLController.Rand.nextDouble();
+            final double r = CIRCLE_RADIUS * java.lang.Math.sqrt(seed);
+
+            final double offset_x = r * java.lang.Math.sin(theta_radians);
+            final double offset_y = r * java.lang.Math.cos(theta_radians);
+
+            final double coord_x = offset_x + CIRCLE_RADIUS;
+            final double coord_y = offset_y + CIRCLE_RADIUS;
+
+            // Take the chosen point and draw it using a small circle,
+            // of diameter 3.0. Careful to offset by 1.0 (*not* 1.5).
+            graphCon.fillRect(coord_x - 1.0, coord_y - 1.0, 3.0, 3.0); // x, y, w, h
+        }
+
+    }
+
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
